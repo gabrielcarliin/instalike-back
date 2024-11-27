11f980eb233ba3d7c24af74c08f3c57b25499010
gabrielcarliin <75861006+gabrielcarliin@users.noreply.github.com>
2024-11-26 21:09:11 -0300

    Finalizado curso imersão dev alura node.js

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,14 +1,28 @@
+import { ObjectId } from "mongodb";
 import conectarAoBanco from "../config/dbConfig.js";
 
 // Conecta ao banco de dados utilizando a string de conexão fornecida como variável de ambiente
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 // Função assíncrona para buscar todos os posts do banco de dados
-export default async function getTodosPosts() {
+export async function getTodosPosts() {
     // Seleciona o banco de dados 'imersao-instabyte'
     const db = conexao.db("imersao-instabyte");
     // Seleciona a coleção 'posts' dentro do banco de dados
     const colecao = db.collection("posts");
     // Executa a operação de busca e retorna todos os documentos como um array
     return colecao.find().toArray();
+}
+
+export async function criarPost(novoPost) {
+    const db = conexao.db("imersao-instabyte");
+    const colecao = db.collection("posts");
+    return colecao.insertOne(novoPost);
+}
+
+export async function atualizarPost(id, novoPost) {
+    const db = conexao.db("imersao-instabyte");
+    const colecao = db.collection("posts");
+    const objId = ObjectId.createFromHexString(id)
+    return colecao.updateOne({_id: new ObjectId(objId)}, {$set:novoPost});
 }
